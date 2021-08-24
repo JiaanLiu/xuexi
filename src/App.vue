bdddd835d9be0d5f897fd0423db9f7edce0458bf
JiaanLiu <5426899770@qq.com>
2021-08-24 19:18:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <main-tab-bar v-if="$route.meta.footShow"></main-tab-bar>
+
+    <router-view v-slot="{ Component }">
+      <keep-alive exclude="Detail">
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
+</template>
+
+<script>
+import MainTabBar from "@/components/content/Maintabbar";
+
+export default {
+  name: "App",
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
+@import url(./assets/css/base.css);
+</style>
